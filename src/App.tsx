c99ecff76f080854c1eaa88f4d6ac03a668090f1
ufombam <ufombam@gmail.com>
2023-09-04 14:48:55 +0100

    nav handleer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/Nav';
+import Nav from './components/Nav/Nav';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Dashboard() {
+  return (
+    <div className="">
+      HOOLA AT ME
+    </div>
+  );
+}
+
+export default Dashboard;
