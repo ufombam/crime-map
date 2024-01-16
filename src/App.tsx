fa69bd317e7824b9e8e35ddac26b07306fe2b94e
ufombam <ufombam@gmail.com>
2024-01-16 19:48:09 +0100

    adding firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav/Nav';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Login from './components/Login/Login';
+
 
 
 function App() {
   return (
-    <div className="">
-      <Nav />
-    </div>
+    <Routes>
+      <Route index element={<Nav />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path="*" element={<h1>Nothing to see here</h1>} />
+    </Routes>
+      
   );
 }
 
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzmddSK6BoUzi3UNzAs8umx2UZ0KGnNcc",
+  authDomain: "crime-map-93121.firebaseapp.com",
+  projectId: "crime-map-93121",
+  storageBucket: "crime-map-93121.appspot.com",
+  messagingSenderId: "2015681387",
+  appId: "1:2015681387:web:d25958a3bc5c98a51d4a2e",
+  measurementId: "G-4FF787HPGR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 
 function Login() {
     return (
         <div className="App">
-            Login
+            This is the login page
         </div>
     );
 }
