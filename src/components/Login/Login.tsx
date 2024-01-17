d237eb2fa07c1c1b5c54343be8ed5a3c8946b6a1
ufombam <ufombam@gmail.com>
2024-01-17 19:36:55 +0100

    adding the auth

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,29 +1,87 @@
 import React from 'react';
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCzmddSK6BoUzi3UNzAs8umx2UZ0KGnNcc",
-  authDomain: "crime-map-93121.firebaseapp.com",
-  projectId: "crime-map-93121",
-  storageBucket: "crime-map-93121.appspot.com",
-  messagingSenderId: "2015681387",
-  appId: "1:2015681387:web:d25958a3bc5c98a51d4a2e",
-  measurementId: "G-4FF787HPGR"
+    apiKey: "AIzaSyCzmddSK6BoUzi3UNzAs8umx2UZ0KGnNcc",
+    authDomain: "crime-map-93121.firebaseapp.com",
+    projectId: "crime-map-93121",
+    storageBucket: "crime-map-93121.appspot.com",
+    messagingSenderId: "2015681387",
+    appId: "1:2015681387:web:d25958a3bc5c98a51d4a2e",
+    measurementId: "G-4FF787HPGR"
 };
 
+//var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            scopes: [
+                'https://www.googleapis.com/auth/contacts.readonly'
+              ],
+              customParameters: {
+                // Forces account selection even when one account
+                // is available.
+                prompt: 'select_account'
+              }
+        },
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    // Other config options...
+  });
+    //ui configuration file
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult: any, redirectUrl: any) {
+                console.log(authResult)
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            //document.getElementById('loader').style.display = 'none';
+        }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: '/',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+    };
+// The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig)
+    
+
 
 
 
 function Login() {
     return (
-        <div className="App">
-            This is the login page
+        <div className="">
+            <h1>Welcome to My Awesome App</h1>
+            <div id="firebaseui-auth-container"></div>
+            <div id="loader">Loading...</div>
         </div>
     );
 }
